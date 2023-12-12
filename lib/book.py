ba792b1e02fd4278bcad2ca1865aca2f08b8728e
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-12 11:03:16 -0800

    passing tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python3
 
+
 class Book:
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+        self._current_page = 1
+
+    @property
+    def title(self):
+        """The title property"""
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, count):
+        """page count must be an integer"""
+        if isinstance(count, int):
+            self._page_count = count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        """Turns a page"""
+        if self._current_page < self._page_count:
+            self._current_page += 1
+            print("Flipping the page...wow, you read fast!")
+        else:
+            print("You've reached the end of the book.")
+
     pass
-    
-        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
+    def __init__(self, brand, size, cobble=None):
+        self._brand = brand
+        self._size = size
+        self._condition = "New"
+
+    @property
+    def brand(self):
+        """The brand property"""
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        """Repairs shoe and sets cobble to 'New'"""
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+
+    @property
+    def condition(self):
+        """The condition property"""
+        return self._condition
+
     pass
